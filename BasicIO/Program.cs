5fa7c4e82cd5dae5acdc3c5e42c0cbefc856f79e
Justyn Durnford <jpdurnford@alaska.edu>
2021-02-11 09:32:06 -0900

    -First Program, basic input/output.

---
+++ BasicIO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_str;
+            Console.WriteLine("Input some text.");
+            input_str = Console.ReadLine();
+            Console.Write("You wrote: ");
+            Console.WriteLine(input_str);
+        }
+    }
+}
